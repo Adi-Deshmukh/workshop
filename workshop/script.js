0e9cb96077b52500ba10e3a00630f09334774057
Adi-Deshmukh <68903154+Adi-Deshmukh@users.noreply.github.com>
2024-11-24 11:18:31 +0530

    Add files via upload

---
+++ workshop/script.js
@@ -0,0 +1,7 @@
+// Responsive Navbar
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
